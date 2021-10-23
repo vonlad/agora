339b592848291aa03285f15faad50e55d1a2c5b7
damien <damien.vigot@protonmail.com>
2021-10-23 17:29:44 +0200

    second commit +

--- reactapp/src/Accueil.js
+++ reactapp/src/Accueil.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Button } from 'antd';
+import './App.css';
 // import {Redirect} from 'react-router-dom';
 // import {connect} from 'react-redux';
 
 function Accueil(props) {
     return (
-        <div>Voici la page d'accueil!</div>
+        <div>
+            <div>Voici la page d'accueil!</div>
+            <Button type="primary" >Boutton</Button>
+        </div>
     )
 }
 
